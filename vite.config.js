b0073d0a6c11664fedee02fef25104234a128422
Manjeet <manjeetman38@gmail.com>
2024-10-05 22:36:59 +0530

    deploying project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',            // Bind to 0.0.0.0 for external access
+    port: process.env.FRONTEND_PORT || 5173  // Change the frontend port, 5173 is default for Vite
+  }
 })
